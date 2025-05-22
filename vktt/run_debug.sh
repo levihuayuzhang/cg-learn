bea083f3787f93e613d9508b9ae43dbdc0aeeba3
Huayu Zhang <zhanghuayu.dev@gmail.com>
2025-05-23 00:13:37 +0800

    logical device and queues
    Signed-off-by: Huayu Zhang <zhanghuayu.dev@gmail.com>

--- vktt/run_debug.sh
+++ vktt/run_debug.sh
@@ -5,7 +5,9 @@ cmake -S ./ -B build \
     -G Ninja \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
     -DCMAKE_BUILD_TYPE=DEBUG \
-    # -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
+    -DUPDATE_DEPS=ON
+# -DCMAKE_PREFIX_PATH=~/projects/c++/tinyobjloader/install
+# -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ \
 
 cmake --build build -j$(nproc) -v
 
--- vktt/src/main.cpp
+++ vktt/src/main.cpp
@@ -62,6 +62,8 @@ class HelloTriangelApplication {
     VkInstance instance;
     VkDebugUtilsMessengerEXT debugMessenger;
     VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+    VkDevice device;
+    VkQueue graphicQueue;
 
     struct QueueFamilyIndices {
         std::optional<uint32_t> graphicsFamily;
@@ -82,6 +84,7 @@ class HelloTriangelApplication {
         createInstance();
         setupDebugMessenger();
         pickPhysicalDevice();
+        createLogicalDevice();
     }
 
     void mainLoop() {
@@ -91,6 +94,8 @@ class HelloTriangelApplication {
     }
 
     void cleanup() {
+        vkDestroyDevice(device, nullptr);
+
         if (enableValidationLayers) {
             DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
         }
@@ -380,6 +385,43 @@ class HelloTriangelApplication {
                   << std::endl;
 
         return VK_FALSE;
+    }
+
+    void createLogicalDevice() {
+        QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+
+        VkDeviceQueueCreateInfo queueCreateInfo{};
+        queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+        queueCreateInfo.queueCount = 1;
+
+        float queuePriority = 1.0f;
+        queueCreateInfo.pQueuePriorities = &queuePriority;
+
+        VkPhysicalDeviceFeatures deviceFeatures{};
+
+        VkDeviceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+        createInfo.pQueueCreateInfos = &queueCreateInfo;
+        createInfo.queueCreateInfoCount = 1;
+        createInfo.pEnabledFeatures = &deviceFeatures;
+        createInfo.enabledExtensionCount = 0;
+
+        if (enableValidationLayers) {
+            createInfo.enabledLayerCount =
+                static_cast<uint32_t>(validationLayers.size());
+            createInfo.ppEnabledLayerNames = validationLayers.data();
+        } else {
+            createInfo.enabledLayerCount = 0;
+        }
+
+        if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) !=
+            VK_SUCCESS) {
+            throw std::runtime_error("failed to create logical device!");
+        }
+
+        vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0,
+                         &graphicQueue);
     }
 };
 
